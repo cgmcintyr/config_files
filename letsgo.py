ee314aafa6f4ba0542dbab1699f0afa0f78699f2
Christopher McIntyre <chrsintyre@gmail.com>
2015-08-30 23:47:48 +0100

    Stopped python script copying repo related files, removed prompt function.

--- letsgo.py
+++ letsgo.py
@@ -8,33 +8,18 @@ import sys
 import shutil
 import distutils.dir_util
 
-PY3 = (sys.version_info >= (3, 0))
 HOME = os.environ['HOME']
 
-if not PY3:
-    input = raw_input
+IGNORE = (
+    '.git', 'letsgo.py', '.gitignore', '.gitmodules', 'README.md', 'bash.bashrc'
+)
 
 
-def prompt_yes_no(question):
-    "Ask user question confirming action to be taken"
-    valid = {'yes': True, 'y': True,
-             'no': False, 'n': False}
-
-    while True:
-        print(question, end=' ')
-        answer = input().lower()
-
-        if (answer == 'yes') or (answer == 'y'):
-            return True
-        elif (answer == 'no') or (answer == 'n'):
-            return False
-        else:
-            print('Please answer yes or no.')
-
-def copy_dir_contents(src, dst, ignore=None):
+def copy_dir_contents(src, dst, ignore=IGNORE):
     "Copies all folders and files under src directory to dst"
     for f in os.listdir(src):
-        if f == 'letsgo.py':
+        if f in ignore:
+            chalk.yellow("Ignoring {0}".format(f))
             continue
 
         fpath = os.path.join(os.getcwd(), f)
@@ -48,6 +33,7 @@ def copy_dir_contents(src, dst, ignore=None):
         else:
             chalk.green("Copying file '{0}'".format(f))
             shutil.copy(fpath, dst)
+
 
 if __name__ == '__main__':
     chalk.white("Copying contents of '{0}' to '{1}'...".format(os.getcwd(), HOME))
