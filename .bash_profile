d97e5412f2acff865ab5d4922883ae4b663f7f50
Christopher McIntyre <chrsintyre@gmail.com>
2016-06-18 23:05:21 +0100

    Updated aliases

--- .bash_profile
+++ .bash_profile
@@ -6,3 +6,4 @@
 
 export PATH=$PATH:~/opt/bin
 
+pyarchey
--- bashscripts/30_bash_aliases.sh
+++ bashscripts/30_bash_aliases.sh
@@ -5,6 +5,9 @@ alias ll='ls -Fl --color=auto'
 alias la='ls -aFl --color=auto'
 alias tree='tree -CF'
 
+# C
+alias gcc='gcc -Wall -Werror -std=c99 -L/usr/local/lib -I/usr/local/include'
+
 # Django
 alias dj='python manage.py'
 alias djrun='python manage.py runserver'
@@ -22,3 +25,11 @@ alias closeport='sudo iptables -A INPUT -p tcp -j DROP --dport'
 
 # Kick network-manager
 alias eduroam='sudo service network-manager restart'
+
+# Search aliases
+alias strgrep='grep -Ril'
+
+# Docker
+alias dcleancontainers 'docker ps -a | grep 'weeks ago' | awk '{print $1}' | xargs --no-run-if-empty docker rm'
+alias dcleanimages 'docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi'
+alias dclean 'dcleancontainers && dcleanimages'
